0808c2ec37a70933b0f4eecf15c9ca4a4e00759e
muneebhashone <muneeb.hashone@gmail.com>
2021-11-17 17:10:07 +0500

    Connected to live server

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import UserProvider from 'src/Context/UserContext';
 
-axios.defaults.baseURL = 'http://localhost:8000/api/v1';
+axios.defaults.baseURL = 'http://178.79.129.18/api/v1';
+// axios.defaults.baseURL = 'http://localhost:9000/api/v1';
 const queryClient = new QueryClient();
 
 const App = () => {
